a0e0beeb9b5491b546f6b19caea1234d41284b5b
Sergiolm17 <kingsergiolm17@gmail.com>
2020-12-25 21:12:54 -0500

    first

--- boardgame.js
+++ boardgame.js
@@ -1,7 +1,8 @@
 const { Server } = require("boardgame.io/server");
+
 const TicTacToe = require("./game");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-const server = Server({ games: [TicTacToe] });
+const server = Server({ games: [TicTacToe.default] });
 
-server.run(PORT);
+server.run(PORT, () => console.log(PORT));
--- game.js
+++ game.js
@@ -1,45 +1,68 @@
-const { data } = require("./data");
+import { deck } from "./data.js";
+import { INVALID_MOVE } from "boardgame.io/core";
+
 function IsVictory(cells, numPlayers) {
   if (cells.filter((a) => Boolean(a)).length === numPlayers - 1) {
-    return true;
+    return false;
   }
   return false;
 }
+const numberdeck = 10;
 
-exports.TicTacToe = {
+const TicTacToe = {
   name: "tic-tac-toe",
 
-  setup: () => ({
-    cells: Array(data.length).fill(null),
-    deck: data,
-    boss: "0",
+  setup: (ctx) => ({
+    //cells: Array(deck.length).fill(null),
+    deck,
+    playerdeck: ShuffleNumber(ctx),
+    select: [],
+    boss: ctx.random.Die(ctx.numPlayers - 1).toString(),
+    max: numberdeck,
   }),
 
   moves: {
     clickCell(G, ctx, id) {
-      //console.log(ctx.numPlayers);
-      if (G.cells[id] === null) {
-        G.cells[id] = ctx.currentPlayer;
+      var newArray = G.select.filter((card) => {
+        return card.player === ctx.currentPlayer;
+      });
+
+      //if (newArray.length === 1) return INVALID_MOVE;
+      console.log("listo");
+      G.select.push({
+        player: ctx.currentPlayer,
+        card: id,
+      });
+    },
+    shuffle(G, ctx) {
+      G.playerdeck = ShuffleNumber(ctx, G);
+    },
+    nextBoss(G, ctx) {
+      if (G.boss === Number(ctx.numPlayers)) {
+        G.boss = "0";
       }
+      G.boss = ctx.random.Die(ctx.numPlayers - 1).toString();
     },
   },
 
   turn: { moveLimit: 1 },
 
   endIf: (G, ctx) => {
+    /*
     if (IsVictory(G.cells, ctx.numPlayers)) {
       return { winner: ctx.currentPlayer };
     }
-    if (IsVictory(G.cells)) {
+    if (IsVictory(G.cells, ctx.numPlayers)) {
       return { draw: true };
     }
+    */
   },
 
   ai: {
     enumerate: (G) => {
       let moves = [];
-      for (let i = 0; i < 9; i++) {
-        if (G.cells[i] === null) {
+      for (let i = 0; i < deck.length; i++) {
+        if (G.deck[i] === null) {
           moves.push({ move: "clickCell", args: [i] });
         }
       }
@@ -47,3 +70,16 @@ exports.TicTacToe = {
     },
   },
 };
+function ShuffleNumber(ctx) {
+  let temp_deck_all = [];
+  let shuffle_deck = ctx.random.Shuffle(Array.from(Array(deck.length).keys()));
+  Array.from(Array(ctx.numPlayers).keys()).forEach((player) => {
+    let temp_deck = [];
+    Array.from(Array(numberdeck).keys()).forEach((deck) => {
+      temp_deck.push(shuffle_deck[player * numberdeck + deck]);
+    });
+    temp_deck_all.push(temp_deck);
+  });
+  return temp_deck_all;
+}
+export default TicTacToe;
