2a61178de0b40dbd0e3abdf59a46aacde2b82cb8
Sergiolm17 <kingsergiolm17@gmail.com>
2021-04-02 20:44:22 -0500

    make it better

--- services/database_firebase.js
+++ services/database_firebase.js
@@ -2,38 +2,44 @@ const superagent = require("superagent");
 var { config } = require("../variables");
 
 async function CreateRoom(sessionID, RoomID, DisplayName, admin = false) {
-  await superagent
-    .put(`${config.urlsocket}/Rooms/${sessionID}.json`)
-    .send({ sessionID, RoomID, DisplayName, admin });
+    await superagent
+        .put(`${config.urlsocket}/Rooms/${sessionID}.json`)
+        .send({ sessionID, RoomID, DisplayName, admin });
+}
+async function UpdateAdmin(sessionID, admin = false) {
+    await superagent
+        .put(`${config.urlsocket}/Rooms/${sessionID}.json`)
+        .send({ admin });
 }
 async function UpdateDisplayName(sessionID, DisplayName) {
-  await superagent
-    .put(`${config.urlsocket}/Rooms/${sessionID}.json`)
-    .send({ sessionID, DisplayName });
+    await superagent
+        .put(`${config.urlsocket}/Rooms/${sessionID}.json`)
+        .send({ sessionID, DisplayName });
 }
 
 async function DeleteSessionID(sessionID) {
-  await superagent.delete(`${config.urlsocket}/Rooms/${sessionID}.json`);
+    await superagent.delete(`${config.urlsocket}/Rooms/${sessionID}.json`);
 }
 
 async function GetRoom(RoomID) {
-  const { body } = await superagent.get(
-    `${config.urlsocket}/Rooms.json?orderBy="RoomID"&equalTo="${RoomID}"`
-  );
-  return body;
+    const { body } = await superagent.get(
+        `${config.urlsocket}/Rooms.json?orderBy="RoomID"&equalTo="${RoomID}"`
+    );
+    return body;
 }
 
 async function GetSession(sessionID) {
-  const { body } = await superagent.get(
-    `${config.urlsocket}/Rooms/${sessionID}.json`
-  );
-  return body;
+    const { body } = await superagent.get(
+        `${config.urlsocket}/Rooms/${sessionID}.json`
+    );
+    return body;
 }
 
 module.exports = {
-  CreateRoom,
-  UpdateDisplayName,
-  DeleteSessionID,
-  GetRoom,
-  GetSession,
+    DeleteSessionID,
+    CreateRoom,
+    UpdateDisplayName,
+    UpdateAdmin,
+    GetRoom,
+    GetSession,
 };
--- services/routes.socket.js
+++ services/routes.socket.js
@@ -1,110 +1,109 @@
 const randomstring = require("randomstring");
 const {
-  DeleteSessionID,
-  CreateRoom,
-  UpdateDisplayName,
-  UpdateAdmin,
-  GetRoom,
-  GetSession,
+    DeleteSessionID,
+    CreateRoom,
+    UpdateDisplayName,
+    UpdateAdmin,
+    GetRoom,
+    GetSession,
 } = require("./database_supabase");
 const lenght_code = 4;
 module.exports = async (socket, io) => {
-  const sessionID = socket.id;
-  //console.log("a user connected ", sessionID);
+    const sessionID = socket.id;
+    //console.log("a user connected ", sessionID);
 
-  socket.on("disconnecting", disconnecting);
-  socket.on("createRoom", createRoom);
-  socket.on("joinRoom", joinRoom);
-  socket.on("leaveRoom", leaveRoom);
+    socket.on("disconnecting", disconnecting);
+    socket.on("createRoom", createRoom);
+    socket.on("joinRoom", joinRoom);
+    socket.on("leaveRoom", leaveRoom);
+    socket.on("eventRoom", eventRoom);
 
-  socket.on("eventRoom", eventRoom);
+    async function disconnecting(msg) {
+        let [Room] = await GetSession(sessionID);
+        if (Room) {
+            await DeleteSessionID(sessionID);
+            let list = await GetRoom(Room.RoomID);
+            await UpdateAdmin(list[0].sessionID, true);
+            console.log(list);
 
-  async function disconnecting(msg) {
-    let [Room] = await GetSession(sessionID);
-    if (Room) {
-      await DeleteSessionID(sessionID);
-      let list = await GetRoom(Room.RoomID);
-      await UpdateAdmin(list[0].sessionID, true);
-      console.log(list);
-
-      socket.broadcast.to(Room.RoomID).emit("playersUpdated", list);
-    }
-    socket.leaveAll();
-  }
-
-  async function createRoom({ DisplayName }) {
-    let [Room] = await GetSession(sessionID);
-    if (Room) {
-      let list = await GetRoom(Room.RoomID);
-      socket.join(Room.RoomID);
-      socket.emit("roomJoined", {
-        RoomID: Room.RoomID,
-        players: list,
-      });
-    } else {
-      var RoomID = randomstring.generate({
-        length: lenght_code,
-        charset: "numeric",
-      });
-      let Room = await CreateRoom(sessionID, RoomID, DisplayName, true);
-      socket.join(RoomID.toString());
-      socket.emit("roomJoined", {
-        RoomID: RoomID.toString(),
-        players: Room,
-      });
-    }
-  }
-
-  async function joinRoom({ RoomID, DisplayName }) {
-    //Comprobar si el codigo es correcto
-    if (RoomID.length !== lenght_code) {
-      socket.emit("roomBadId");
-      return;
-    }
-    //Buscar en BD si uniste al grupo
-    let list = await GetRoom(RoomID);
-    if (list.filter((e) => e.sessionID === sessionID).length) {
-      console.log("Ya te uniste a este grupo");
-      await UpdateDisplayName(sessionID, DisplayName);
-      return;
+            socket.broadcast.to(Room.RoomID).emit("playersUpdated", list);
+        }
+        socket.leaveAll();
     }
 
-    if (Object.keys(list).length > 0) {
-      console.log(
-        "Usuarios activos en la habitación ",
-        Object.keys(list).length
-      );
-      let Room = await CreateRoom(sessionID, RoomID, DisplayName);
-      socket.join(RoomID);
-      let players = [...Room, ...list];
-      socket.emit("roomJoined", {
-        RoomID: RoomID.toString(),
-        players,
-      });
-      socket.broadcast.to(RoomID).emit("playersUpdated", players);
-    } else {
-      socket.emit("roomBadId");
+    async function createRoom({ DisplayName }) {
+        let [Room] = await GetSession(sessionID);
+        if (Room) {
+            let list = await GetRoom(Room.RoomID);
+            socket.join(Room.RoomID);
+            socket.emit("roomJoined", {
+                RoomID: Room.RoomID,
+                players: list,
+            });
+        } else {
+            var RoomID = randomstring.generate({
+                length: lenght_code,
+                charset: "numeric",
+            });
+            let Room = await CreateRoom(sessionID, RoomID, DisplayName, true);
+            socket.join(RoomID.toString());
+            socket.emit("roomJoined", {
+                RoomID: RoomID.toString(),
+                players: Room,
+            });
+        }
     }
-  }
 
-  async function leaveRoom({ RoomID }) {
-    if (RoomID) {
-      socket.leave(RoomID);
-    } else {
-      let [Room] = await GetSession(sessionID);
-      if (Room) {
-        await DeleteSessionID(sessionID);
-        let list = await GetRoom(Room.RoomID);
-        socket.broadcast.to(Room.RoomID).emit("playersUpdated", list);
-      }
-      socket.leaveAll();
-    }
-  }
+    async function joinRoom({ RoomID, DisplayName }) {
+        //Comprobar si el codigo es correcto
+        if (RoomID.length !== lenght_code) {
+            socket.emit("roomBadId");
+            return;
+        }
+        //Buscar en BD si uniste al grupo
+        let list = await GetRoom(RoomID);
+        if (list.filter((e) => e.sessionID === sessionID).length) {
+            console.log("Ya te uniste a este grupo");
+            await UpdateDisplayName(sessionID, DisplayName);
+            return;
+        }
 
-  async function eventRoom({ RoomID, msg }) {
-    if (RoomID) {
-      console.log({ RoomID, msg });
-      socket.broadcast.to(RoomID).emit("eventRoom", msg);
+        if (Object.keys(list).length > 0) {
+            console.log(
+                "Usuarios activos en la habitación ",
+                Object.keys(list).length
+            );
+            let Room = await CreateRoom(sessionID, RoomID, DisplayName);
+            socket.join(RoomID);
+            let players = [...Room, ...list];
+            socket.emit("roomJoined", {
+                RoomID: RoomID.toString(),
+                players,
+            });
+            socket.broadcast.to(RoomID).emit("playersUpdated", players);
+        } else {
+            socket.emit("roomBadId");
+        }
     }
-  }
+
+    async function leaveRoom({ RoomID }) {
+        if (RoomID) {
+            socket.leave(RoomID);
+        } else {
+            let [Room] = await GetSession(sessionID);
+            if (Room) {
+                await DeleteSessionID(sessionID);
+                let list = await GetRoom(Room.RoomID);
+                socket.broadcast.to(Room.RoomID).emit("playersUpdated", list);
+            }
+            socket.leaveAll();
+        }
+    }
+
+    async function eventRoom({ RoomID, msg }) {
+        if (RoomID) {
+            console.log({ RoomID, msg });
+            socket.broadcast.to(RoomID).emit("eventRoom", msg);
+        }
+    }
 };
